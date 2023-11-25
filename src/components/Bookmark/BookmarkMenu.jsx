c22e06410b0680fa1ddb851f6bc4dae4665feb46
NK7 <frostbreker@gmail.com>
2023-11-25 17:08:09 +0100

    Fix unused var

--- src/components/Bookmark/BookmarkMenu.jsx
+++ src/components/Bookmark/BookmarkMenu.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import AddBookmarkButton from '../Buttons/AddBookmarkButton'
 import BookmarkItem from './BookmarkItem'
 import { useSelector } from 'react-redux'
 
-export default function BookmarkMenu ({ showBookmarksMenu, handleOpenWebsite, handleOpenNewTabFromBookmark, handleShowModals }) {
+export default function BookmarkMenu({ showBookmarksMenu, handleOpenWebsite, handleOpenNewTabFromBookmark, handleShowModals }) {
   const bookmarks = useSelector(state => state.bookmarksReducer)
 
   return (
     <div className='bookmarksMenu' style={{ display: showBookmarksMenu ? 'flex' : 'none' }}>
       <AddBookmarkButton />
       {
-                bookmarks.map((bookmark, index) => (
-                  <BookmarkItem key={index} book={bookmark} handleOpenWebsite={handleOpenWebsite} handleOpenNewTabFromBookmark={handleOpenNewTabFromBookmark} handleShowModals={handleShowModals} />
-                ))
-            }
+        bookmarks.map((bookmark, index) => (
+          <BookmarkItem key={index} book={bookmark} handleOpenWebsite={handleOpenWebsite} handleOpenNewTabFromBookmark={handleOpenNewTabFromBookmark} handleShowModals={handleShowModals} />
+        ))
+      }
     </div>
   )
 }
